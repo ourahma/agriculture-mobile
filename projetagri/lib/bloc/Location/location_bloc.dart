fc209dc17d21ee9d017cc404f89d9efc37913647
NassimaZENNOURI <122114023+NassimaZENNOURI@users.noreply.github.com>
2023-05-27 11:54:47 +0100

    Update location_bloc.dart

--- projetagri/lib/bloc/Location/location_bloc.dart
+++ projetagri/lib/bloc/Location/location_bloc.dart
@@ -2,27 +2,27 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-
 import '../../Reposistory/Geolocation/geolocationRepository.dart';
+import '../../Reposistory/Geolocation/places.dart';
 
 part 'location_event.dart';
 part 'location_state.dart';
 
 class LocationBloc extends Bloc<LocationEvent, LocationState> {
   GeolocationRepository repo;
-  LocationBloc(OnpressedLocationLoadingState initialLoading, this.repo)
-      : super(OnpressedLocationLoadingState()) {
+  LocationBloc(OnpressedLocationLoadingState initialLoading, this.repo) : super(OnpressedLocationLoadingState()) {
     on<LocationEvent>((event, emit) async {
-      if (event is OnpressedLocationEvent) {
+      if (event is OnpressedLocationPositionEvent || event is OnpressedLocationSearchEvent) {
         emit(OnpressedLocationLoadingState());
         Position position = await  repo.determinePosition();
-        List<Placemark> address= await repo.getLocation(position);
+        Places place =Places();
+        List<Placemark> address= await place.getLocation(position);
         if (position == LocationPermission.denied || position == LocationPermission.deniedForever) {
           print("Permission denied ");
           emit(OnpressedLocationErrorState(LocationPermission.denied.toString()));
         }else{
-          emit(OnpressedLocationLoadedState(position,address ));
-        } 
+          emit(OnpressedLocationLoadedState(position,address));
+        }
       }
     });
   }
