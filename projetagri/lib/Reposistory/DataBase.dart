ec5902a3398be35e389009ba7526d037ca2c7270
ourahmamaroua <122113141+ourahma@users.noreply.github.com>
2024-09-15 10:43:26 +0000

    Update DataBase.dart

--- projetagri/lib/Reposistory/DataBase.dart
+++ projetagri/lib/Reposistory/DataBase.dart
@@ -7,14 +7,10 @@ import 'package:mongo_dart/mongo_dart.dart';
 class Database {
   static Future<Db> connect() async {
     var db = null;
-    /*
-    * mongodb+srv://maroua:1234@cluster0.fiyvmrf.mongodb.net/agricultureapp?retryWrites=true&w=majority
-
-    mongodb+srv://groupe:eagriculture@cluster0.zicrcop.mongodb.net/test
-    * */
+   
     if (db == null) {
       db = await Db.create(
-          'mongodb+srv://maroua:1234@cluster0.fiyvmrf.mongodb.net/agricultureapp?retryWrites=true&w=majority');
+          'link');
       await db.open(); //open the connection
       inspect(db);
       var statut = db.serverStatus();
